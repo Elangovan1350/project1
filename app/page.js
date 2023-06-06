5852d47b2d58cd973eb6e0811f108aa063dec6ce
John Doe <johndoe@example.com>
2023-06-06 21:50:41 +0530

    b

--- app/page.js
+++ app/page.js
@@ -4,6 +4,9 @@ export default function Home() {
   return (
     <main className="flex ">
       <div className='h-screen w-screen bg-slate-500'>
+        <h1>
+          Elangovan
+        </h1>
 
       </div>
      
